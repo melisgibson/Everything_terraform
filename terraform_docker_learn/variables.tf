79eb1a0cfb828319f1cc7a2172545c39e3c3e038
melisgibson <104530423+melisgibson@users.noreply.github.com>
2022-08-11 14:33:54 -0500

    Update variables.tf

--- terraform_docker_learn/variables.tf
+++ terraform_docker_learn/variables.tf
@@ -1,11 +1,26 @@
+variable "image" {
+type = map
+description = "image for container"
+default = {
+  dev =  "nodered/node-red:latest"
+  prod = "nodered/node-red:latest-minimal"
+  }
+}
+
 variable "ext_port" {
-  type = list
+  type = map
 
 validation {
-  condition = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 0
+  condition = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+  error_message = "The external port must be in the vaild port rand 0 - 65535."
+ }
+ 
+validation {
+  condition = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
   error_message = "The external port must be in the vaild port rand 0 - 65535."
  }
 }
+
 
 variable "int_port" {
   type = number 
@@ -18,5 +33,5 @@ variable "int_port" {
 }
 
 locals {
-  container_count = length(var.ext_port)
+  container_count = length(var.ext_port[terraform.workspace])
 }
